e1442a01967b44b792b4de0687205e09a414a2f6
alfonsoulloao <alfonsoulloa.o@gmail.com>
2023-12-22 16:07:03 -0300

    se crea componente de dona

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 // modulos
 import { FormsModule } from '@angular/forms';
 // componentes
 import { IncrementadorComponent } from './incrementador/incrementador.component';
+import { DonaComponent } from './dona/dona.component';
 
 
 @NgModule({
   declarations: [
-    IncrementadorComponent
+    IncrementadorComponent,
+    DonaComponent
   ],
   imports: [
     CommonModule,
     FormsModule
   ],
   exports:[
-    IncrementadorComponent
+    IncrementadorComponent,
+    DonaComponent
   ]
 })
 export class ComponentsModule { }
